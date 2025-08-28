2db6274da1003811b826e87bcebd88761ef42bc9
RustamMust <rustammustavaev@gmail.com>
2025-08-28 08:36:27 +0500

    no_task: add class CostCounter

---
+++ src/practice_7/restaurant_manager/Main.java
@@ -0,0 +1,7 @@
+package practice_7.restaurant_manager;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/practice_7/restaurant_manager/RestaurantManager.java
@@ -0,0 +1,7 @@
+package practice_7.restaurant_manager;
+
+public class RestaurantManager {
+    // добавлять заказ в конец очереди
+    // обрабатывать заказ из начала очереди
+    // удалять заказ в любом месте очереди
+}
