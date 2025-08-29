0826ef3c4211ea9a73ec46de5a8c315c40170554
RustamMust <rustammustavaev@gmail.com>
2025-08-29 08:49:35 +0500

    no_task: add class ArrayListEvenNumber

--- src/practice_7/homework/Main.java
+++ src/practice_7/homework/Main.java
@@ -10,6 +10,17 @@ public class Main {
         arrayListAddNumber.printArrayList();
 
 
+        ArrayListEvenNumber arrayListEvenNumber = new ArrayListEvenNumber();
+        arrayListEvenNumber.addNumberToArrayList(6);
+        arrayListEvenNumber.addNumberToArrayList(10);
+        arrayListEvenNumber.addNumberToArrayList(3);
+        arrayListEvenNumber.addNumberToArrayList(5);
+        arrayListEvenNumber.addNumberToArrayList(4);
+        arrayListEvenNumber.addNumberToArrayList(7);
+
+        arrayListEvenNumber.printArrayListEvenNumber();
+
+        arrayListEvenNumber.printAllEvenNumbers();
 
 
     }
