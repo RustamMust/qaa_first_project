190e95328b98deeb3cdb2ae939b236e47dab8e23
RustamMust <rustammustavaev@gmail.com>
2025-08-29 08:39:07 +0500

    no_task: add class ArrayListAddNumber

---
+++ src/practice_7/homework/Main.java
@@ -0,0 +1,16 @@
+package practice_7.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayListAddNumber arrayListAddNumber = new ArrayListAddNumber();
+
+        arrayListAddNumber.printArrayList();
+
+        arrayListAddNumber.addNewNumberInList(6);
+        arrayListAddNumber.printArrayList();
+
+
+
+
+    }
+}
