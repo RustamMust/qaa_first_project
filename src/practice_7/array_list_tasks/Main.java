a4125b25578c73caf6bae01fd985e360fa4ef41c
RustamMust <rustammustavaev@gmail.com>
2025-08-29 08:29:59 +0500

    no_task: add class ArrayListTasks

---
+++ src/practice_7/array_list_tasks/Main.java
@@ -0,0 +1,20 @@
+package practice_7.array_list_tasks;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayListTasks arrayListTasks = new ArrayListTasks();
+
+        arrayListTasks.printArrayList();
+
+        EvenOnly evenOnly = new EvenOnly();
+        evenOnly.addNumbers(15);
+        evenOnly.addNumbers(2);
+        evenOnly.addNumbers(4);
+
+        evenOnly.printList();
+        evenOnly.printEvenNumbers();
+
+    }
+}
