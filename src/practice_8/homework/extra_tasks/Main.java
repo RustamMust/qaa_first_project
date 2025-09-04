6f69804f763baf252a25a7b5b5bd6117218bd7a5
RustamMust <rustammustavaev@gmail.com>
2025-09-04 08:51:08 +0500

    no_task: add printList

--- src/practice_8/homework/extra_tasks/Main.java
+++ src/practice_8/homework/extra_tasks/Main.java
@@ -1,9 +1,11 @@
 package practice_8.homework.extra_tasks;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. Обобщённый класс
         Storage<String> stringStorage = new Storage<>();
         stringStorage.setElement("Хранение");
         System.out.println(stringStorage.getElement());
@@ -12,12 +14,14 @@ public class Main {
         integerStorage.setElement(1);
         System.out.println(integerStorage.getElement());
 
+        // 2. Обобщённый метод
         List<String> strings = List.of("one", "two", "three");
         List<Integer> numbers = List.of(1, 2, 3, 4);
 
         printList(strings);
         printList(numbers);
 
+        // 3. Работа с числами
         NumberBox<Integer> integerNumberBox = new NumberBox<>();
         integerNumberBox.addNumber(1);
         integerNumberBox.addNumber(2);
@@ -33,11 +37,46 @@ public class Main {
 
         System.out.println("Sum of numbers: " + doubleNumberBox.getSum());
 
+        // 4. Ограничение сверху
+        List<Integer> integerList = List.of(1,2,3,4,5);
+        System.out.println(sum(integerList));
+
+        List<Double> doubleList = List.of(1.5, 4.5, 3.5);
+        System.out.println(sum(doubleList));
+
+        // 5. Ограничение снизу
+        List<Number> numbers4 = new ArrayList<>();
+        addNumbers(numbers4);
+
+        for (Object obj : numbers4) {
+            Integer num = (Integer) obj;
+            System.out.println(num);
+        }
+
+
+
+
+
     }
 
     public static <T> void printList(List<T> elements) {
         for (T element : elements) {
             System.out.println(element);
         }
+    }
+
+    public static <T extends Number> double sum(List<T> list) {
+        double total = 0.0;
+
+        for (T element : list) {
+            total+=element.doubleValue();
+        }
+        return total;
+    }
+
+    public static void addNumbers(List<? super Integer> list) {
+        list.add(10);
+        list.add(20);
+        list.add(30);
     }
 }
